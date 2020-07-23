205b0ff0b7ca35552be334665ac8046d48fcd823
Camila Zelenovsky <camila_zele@hotmail.com>
2020-07-23 20:24:47 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace atividadeRecursividade
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Que numero deseja adicionar?");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            int resultado = new Recursivo().Add(number);
+            Console.WriteLine($"A soma é: {resultado}.");
+
+
+
+            //int soma = 0;
+
+            //for (int i = 1; i <= number; i++)
+            //{ 
+            //    soma += i;
+            //}
+
+            //Console.WriteLine($"A soma é: {soma});
+        }
+    }
+
+    public class Recursivo
+    {
+        public int Add(int n)
+        {
+            int soma;
+            if (n == 0)
+                return 0;
+            else
+                soma = n + Add(n - 1);
+            
+            return soma;
+
+        }
+
+
+
+
+
+
+
+    }
+}
